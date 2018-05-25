4596e63ca23d140d334dc3af45a12d1ad55ad4de
BoomerBrian <boomerbrian@gmail.com>
2018-05-25 10:55:15 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -e -x
 cd sonic-eats-ms
   ./gradlew build
 
-cp sonic-eats-ms/build/libs/demo-service-0.0.1-SNAPSHOT.jar ./build-output
+cp build/libs/demo-service-0.0.1-SNAPSHOT.jar ./build-output
